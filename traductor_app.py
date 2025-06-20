3de0431dab1f4125c5f269a7007e017cf546b9fa
mesiast01 <mtorres60036812@gmail.com>
2025-06-20 11:32:44 -0500

    Add files via upload

--- traductor_app.py
+++ traductor_app.py
@@ -36,7 +36,11 @@ authenticator = stauth.Authenticate(
     cookie_expiry_days=config['cookie']['expiry_days']
 )
 
-name, authentication_status, username = authenticator.login('Iniciar sesión', 'main')
+name, authentication_status, username = authenticator.login(
+    form_name='Iniciar sesión',
+    location='main'
+)
+
 
 if authentication_status is False or authentication_status is None:
     with st.expander("¿No tienes cuenta? Regístrate"):
